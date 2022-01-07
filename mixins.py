d676972fd3bde3f683963dcf6dcadebff6158b24
bathelomeo <ngethenathaniel@gmail.com>
2022-01-07 22:05:08 +0400

    Added a new file

---
+++ mixins.py
@@ -0,0 +1,25 @@
+class Loggable:
+    def __init__(self):
+        self.title = ''
+    def log(self):
+        print('Log message from ' + self.title)
+
+class Connection:
+    def __init__(self):
+        self.server = ''
+    def connect(self):
+        print('Connecting to data base on ' + self.server)
+
+class Sqldatabase(Connection , Loggable):
+    def  __init__(self):
+        self.title = 'Sql Connection Demo'
+        self.server = 'Some_Server'
+
+def framework(item):
+    if isinstance(item,Connection):
+        item.connect()
+        isinstance(item , Loggable)
+        item.log()
+
+sql_connection = Sqldatabase()
+framework(sql_connection)
